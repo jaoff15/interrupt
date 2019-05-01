9b320776625b034507f39f9f335913ff6f305338
jaoff15 <jaoff15@student.sdu.dk>
2019-05-01 13:25:59 +0200

    First commit

---
+++ project_1.sdk/project/Debug/src/main.d
@@ -0,0 +1,58 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../project_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_exception.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xscugic.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../project_bsp/ps7_cortexa9_0/include/xil_exception.h
+
+../../project_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../project_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_exception.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xscugic.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../project_bsp/ps7_cortexa9_0/include/xil_exception.h:
